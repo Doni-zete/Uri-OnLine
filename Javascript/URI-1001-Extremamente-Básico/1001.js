a0df1ed9d8fd1888a160f64960c1409fe3972669
Doni-zete <donizete112@gmail.com>
2021-08-10 21:12:18 -0300

    extremamente basico

--- Javascript/URI-1001-Extremamente-Básico/1001.js
+++ Javascript/URI-1001-Extremamente-Básico/1001.js
@@ -1,3 +1,7 @@
+
+//Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+
 let  input = require("fs").readFileSync("stdin", "utf8");
 
 let valores = input.split("\n");
@@ -5,5 +9,5 @@ let valores = input.split("\n");
 var A = parseInt(valores.shift());
 var B = parseInt(valores.shift());
 
-var x=A+B;
+let  x=A+B;
 console.log("X = "+ x);
