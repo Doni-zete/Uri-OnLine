869415bafd7b840b5bcdc4807b7c055662d39a56
Doni-zete <donizete112@gmail.com>
2021-08-10 21:49:28 -0300

    area circulo

--- Javascript/URI-1002-Área-do-Círculo/1002.js
+++ Javascript/URI-1002-Área-do-Círculo/1002.js
@@ -7,7 +7,8 @@ Entrada
 A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
 
 Saída
-Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". */
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). 
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". */
 
 let input = require("fs").readFileSync("stdin", "utf8");
 
